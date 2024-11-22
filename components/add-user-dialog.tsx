c272cad98da815b68cc14b274f73a4a3131a4c9b
Elvan <elvan@transfez.com>
2024-11-22 23:33:17 +0700

    remove label

--- components/add-user-dialog.tsx
+++ components/add-user-dialog.tsx
@@ -10,7 +10,6 @@ import {
   DialogTrigger,
 } from "@/components/ui/dialog"
 import { Input } from "@/components/ui/input"
-import { Label } from "@/components/ui/label"
 import { User } from '@/types'
 import { Plus } from 'lucide-react'
 
